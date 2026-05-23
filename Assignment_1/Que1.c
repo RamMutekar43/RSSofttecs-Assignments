c1f74be7604879ae646e98b618e842ecd747b8fb
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2026-05-23 12:59:42 +0530

    Assignment1

---
+++ Assignment_1/Que1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    // Variable declaration and user input for radius of circle
+    float r;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &r);
+
+    // calculate area of the circle
+    float area = 3.14*r*r;
+    // calculate circumference of the circle
+    float circumference = 2*3.14*r;
+
+    // Print output
+    printf("The area of the circle is: %f", area);
+    printf("\nThe circumference of the circle is: %f", circumference);
+
+    return 0;
+}
---
+++ Assignment_1/Que2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // Variable declaration and user input for radius and height of cylinder
+    float r,h;
+
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f",&r);
+    printf("Enter the height of the cylinder: ");
+    scanf("%f",&h);
+
+    float volume = 3.14*r*r*h; // calculate volume of the cylinder
+    float surfaceArea = (2*3.14*r*h) + (2*3.14*r*r); // calculate surface area of the cylinder
+
+    // Print Volume and Surface Area of Cylinder
+    printf("The volume of the Cylinder is: %f", volume);
+    printf("\n");
+    printf("The surface area of the Cylinder is: %f", surfaceArea);
+    return 0;
+}
