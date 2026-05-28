e67c9b377dfdd065e081030a4d9d6a77d544af9a
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2026-05-28 16:47:05 +0530

    commit

--- Patterns/Pattern10.c
+++ Patterns/Pattern10.c
@@ -2,7 +2,7 @@
 int main(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=5;j++){
-            if(i%2){
+            if(i%2 && j%2){
                 printf("1 ");
             }else{
                 printf("0 ");
