7fae6d867ddf968d6d8c1b5f91f0128b5e0d8fc1
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2026-05-28 16:37:56 +0530

    commit

--- Patterns/Pattern8.c
+++ Patterns/Pattern8.c
@@ -3,9 +3,9 @@ int main(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=5;j++){
             if(j%2){
-                printf("1");
+                printf("1 ");
             }else{
-                printf("0");
+                printf("0 ");
             }
         }
         printf("\n");
