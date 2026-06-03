dd730e2809356306d1c8055e3e7f2b5689be9084
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2026-06-03 09:27:25 +0530

    Commit

--- Patterns/Rhombus.c
+++ Patterns/Rhombus.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
 int main(){
     int k=5;
+    int z=1;
     for(int i=0;i<9;i++){
         int j=1;
         while(j<=9){
-            if(i<=4){
-                if(j>=k-i && j<=k+i){
+            if(i<=4 && j>=k-i && j<=k+i){
                     printf("* ");
                     j++;
-                }else{
-                    printf("  ");
+            }else if(i>4 && j>=i-k+2 && j<=i+k-z*2){
+                    printf("* ");
                     j++;
-                }
             }else{
-                if(j>=k-i && j<=k+i){
-                    printf("* ");
-                    j++;
-                }else{
                     printf("  ");
                     j++;
-                }
             }
 
+        }
+
+        if(i>4){
+            z++;
         }
         printf("\n");
     }
